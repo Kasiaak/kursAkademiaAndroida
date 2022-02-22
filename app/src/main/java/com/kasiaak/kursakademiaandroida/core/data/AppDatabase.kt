eb1e94030c02205513edb4fb650e6e56e2b3d303
Kasiaak <zuzanna.dawiec@gmail.com>
2022-02-22 20:12:41 +0100

    Add app module and database module

---
+++ app/src/main/java/com/kasiaak/kursakademiaandroida/core/data/AppDatabase.kt
@@ -0,0 +1,6 @@
+package com.kasiaak.kursakademiaandroida.core.data
+
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase : RoomDatabase() {
+}
--- app/src/main/java/com/kasiaak/kursakademiaandroida/core/di/KoinInjector.kt
+++ app/src/main/java/com/kasiaak/kursakademiaandroida/core/di/KoinInjector.kt
@@ -3,5 +3,7 @@ package com.kasiaak.kursakademiaandroida.core.di
 import org.koin.core.module.Module
 
 val koinInjector: List<Module> = listOf(
-    networkModule
+    networkModule,
+    appModule,
+    databaseModule
 )
