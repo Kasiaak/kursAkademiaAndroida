2abef279e56350167627439e4cd1ecb2a984282a
Kasiaak <zuzanna.dawiec@gmail.com>
2022-05-04 23:06:50 +0200

    Add bottom navbar with Episodes, characters and locations but it cuts the bottom of Episode fragment

--- app/src/main/java/com/kasiaak/kursakademiaandroida/MainActivity.kt
+++ app/src/main/java/com/kasiaak/kursakademiaandroida/MainActivity.kt
@@ -2,14 +2,39 @@ package com.kasiaak.kursakademiaandroida
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kasiaak.kursakademiaandroida.features.episodes.presentation.EpisodeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavbar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.episode_container, EpisodeFragment())
+        bottomNavbar = findViewById(R.id.bottom_navbar)
+        bottomNavbar.setOnItemReselectedListener {
+            when (it.itemId) {
+                R.id.navbar_episodes -> {
+                    loadFragment(R.id.episode_container, EpisodeFragment())
+                    return@setOnItemReselectedListener
+                }
+                R.id.navbar_characters -> {
+                    // loadFragment()
+                }
+                R.id.navbar_locations -> {
+                    //loadFragment()
+                }
+            }
+        }
+
+    }
+
+    private fun loadFragment(containerViewId: Int, fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(containerViewId, fragment)
+            .addToBackStack(null)
             .commit()
     }
 }
